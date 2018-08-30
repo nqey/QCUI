9c422ae8af6435e0db9bd05ab979831a9b7cd507
nqey <shincyou@hotmail.com>
2018-08-30 14:31:59 +0800

    个人UI框架初建

---
+++ ui/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <v-header/>
+    <div class="container">
+      <v-nav/>
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+    'v-header': () => import('@/components/header'),
+    'v-nav': () => import('@/components/nav')
+  }
+}
+</script>
+
+<style lang="scss">
+  @import 'assets/css/common.scss';
+
+  #app > div.container {
+    margin-top: 80px;
+  }
+
+</style>
---
+++ ui/src/config/api.js
@@ -0,0 +1,54 @@
+import axios from 'axios'
+// import cache from './cache'
+
+const apiSrv = 'http://localhost:3001'
+
+/**
+ * @author 秦超 on 2018年8月23日
+ * @returns agents List
+ */
+export const getAgentsList = async () => {
+  const res = await axios.get(`${apiSrv}/agents`, {
+    // adapter: cache({
+    //   local: false // 是否永久保留在本地，默认为false
+    // })
+  })
+  return res.data
+}
+
+/**
+ * @author 秦超 on 2018年8月23日
+ * @param id
+ * @returns agents
+ */
+export const getAgents = async (id) => {
+  const res = await axios.get(`${apiSrv}/agents/${id}`, {
+    // adapter: cache({
+    //   local: false // 是否永久保留在本地，默认为false
+    // })
+  })
+  return res.data
+}
+
+/**
+ * @author 秦超 on 2018年8月23日
+ * @param id
+ * @returns agents
+ */
+export const upAgents = async (id, data) => {
+  const res = await axios.put(`${apiSrv}/agents/${id}`, data, {
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  })
+  return res.data
+}
+
+/**
+ * @author 秦超 on 2018年8月23日
+ * @returns logs
+ */
+export const getLogs = async () => {
+  const res = await axios.get(`${apiSrv}/logs`)
+  return res.data
+}
